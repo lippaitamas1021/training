613fe08a056f1e7bdf31df626c70ac07e8a12180
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-03-22 10:26:23 +0100

    Circle commit #2

--- src/main/java/numbers/CircleMain.java
+++ src/main/java/numbers/CircleMain.java
@@ -1,10 +1,20 @@
 package numbers;
 
+import java.util.Scanner;
+    
 public class CircleMain {
 
     public static void main(String[] args) {
-        Circle circle1 = new Circle(5);
-        Circle circle2 = new Circle(6);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kérem, adja meg az első kör átmérőjét:");
+        int diameter1 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Kérem, adja meg a második kör átmérőjét:");
+        int diameter2 = scanner.nextInt();
+        scanner.nextLine();
+
+        Circle circle1 = new Circle(diameter1);
+        Circle circle2 = new Circle(diameter2);
 
         System.out.println(circle1.perimeter());
         System.out.println(circle1.area());
