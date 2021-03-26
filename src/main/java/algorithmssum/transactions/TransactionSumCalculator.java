9dbf99e2fcb965262ef454dcf97da2946a5770da
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-03-26 15:30:21 +0100

    algorithmssum.transactions commit

---
+++ src/main/java/algorithmssum/transactions/TransactionSumCalculator.java
@@ -0,0 +1,16 @@
+package algorithmssum.transactions;
+
+import java.util.List;
+
+public class TransactionSumCalculator {
+
+    public int sumAmountOfCreditEntries(List<Transaction> transactions) {
+        int sum = 0;
+        for (Transaction transaction : transactions) {
+            if (transaction.getTransactionOperation().equals(TransactionOperation.CREDIT)) {
+                sum += transaction.getAmount();
+            }
+        }
+        return sum;
+    }
+}
