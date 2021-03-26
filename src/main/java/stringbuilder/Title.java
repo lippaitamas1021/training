851ea21b17c019f4580b9250f4e23c148e5cfdc9
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-03-26 08:21:49 +0100

    stringbuilder NameBuilder commit

---
+++ src/main/java/stringbuilder/Title.java
@@ -0,0 +1,15 @@
+package stringbuilder;
+
+public enum Title {
+    Mr("Mr."), MS("Ms."), DR("Dr."), PROF("Prof.");
+
+    private String titleString;
+
+    Title(String titleString) {
+        this.titleString = titleString;
+    }
+
+    public String getTitleString() {
+        return titleString;
+    }
+}
