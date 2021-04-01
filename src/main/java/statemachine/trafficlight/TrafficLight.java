b28c64eba65dfa352e8656b23b539bc91dd9c1f1
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-04-01 16:39:55 +0200

    statemachine.trafficlight commit

---
+++ src/main/java/statemachine/trafficlight/TrafficLight.java
@@ -0,0 +1,32 @@
+package statemachine.trafficlight;
+
+public enum TrafficLight {
+
+    RED {
+        @Override
+        public TrafficLight next() {
+            return RED_YELLOW;
+        }
+    },
+    RED_YELLOW {
+        @Override
+        public TrafficLight next() {
+            return GREEN;
+        }
+    },
+    GREEN {
+        @Override
+        public TrafficLight next() {
+            return YELLOW;
+        }
+    },
+    YELLOW {
+        @Override
+        public TrafficLight next() {
+            return RED;
+        }
+    };
+
+
+    public abstract TrafficLight next();
+}
