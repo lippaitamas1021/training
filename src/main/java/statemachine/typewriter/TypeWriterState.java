76a66c081bef399698ade931a167f523325a6ccf
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-04-01 16:43:22 +0200

    statemachine.typewriter commit

---
+++ src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,19 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+
+    UPPERCASE {
+        @Override
+        public TypeWriterState next() {
+            return LOWERCASE;
+        }
+    },
+    LOWERCASE {
+        @Override
+        public TypeWriterState next() {
+            return UPPERCASE;
+        }
+    };
+
+    public abstract TypeWriterState next();
+}
