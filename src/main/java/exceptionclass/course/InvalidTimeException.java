e266c877d439a94d8884ae28e442f7c2ae25b28a
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-04-02 08:01:37 +0200

    exceptionclass.course commit

---
+++ src/main/java/exceptionclass/course/InvalidTimeException.java
@@ -0,0 +1,11 @@
+package exceptionclass.course;
+
+public class InvalidTimeException extends RuntimeException {
+
+    public InvalidTimeException(String message) {
+        super(message);
+    }
+
+    public InvalidTimeException() {
+    }
+}
