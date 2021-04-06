8bb9250841689ab831f57350e4debf2ab0c15e2c
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-04-06 09:51:49 +0200

    ioreader.idread commit

---
+++ src/main/java/ioreader/idread/IdManager.java
@@ -0,0 +1,29 @@
+package ioreader.idread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdManager {
+
+    private List<String> ids = new ArrayList<>();
+
+    public List<String> getIds() {
+        return ids;
+    }
+
+    public void readIdsFromFile(String fileName) {
+        Path path = Path.of(fileName);
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                ids.add(line);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Can not read the file", ioe);
+        }
+    }
+}
