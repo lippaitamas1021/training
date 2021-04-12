567097d18adefc422e10c2b884dd83302d8440df
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-04-12 16:54:47 +0200

    clone.timesheet commit

---
+++ src/main/java/clone/timesheet/TimeSheetItem.java
@@ -0,0 +1,49 @@
+package clone.timesheet;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TimeSheetItem {
+
+    private String employee;
+    private String project;
+    private LocalDateTime from;
+    private LocalDateTime to;
+
+    public TimeSheetItem(String employee, String project, LocalDateTime from, LocalDateTime to) {
+        this.employee = employee;
+        this.project = project;
+        this.from = from;
+        this.to = to;
+    }
+
+    public TimeSheetItem(TimeSheetItem anotherTimeSheetItem) {
+        this.employee = anotherTimeSheetItem.employee;
+        this.project = anotherTimeSheetItem.project;
+        this.from = anotherTimeSheetItem.from;
+        this.to = anotherTimeSheetItem.to;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public String getProject() {
+        return project;
+    }
+
+    public LocalDateTime getFrom() {
+        return from;
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+
+    public static TimeSheetItem withDifferentDay(TimeSheetItem timeSheetItem, LocalDate localDate) {
+        TimeSheetItem newTimeSheetItem = new TimeSheetItem(timeSheetItem);
+        newTimeSheetItem.from = LocalDateTime.of(localDate, newTimeSheetItem.from.toLocalTime());
+        newTimeSheetItem.to = LocalDateTime.of(localDate, newTimeSheetItem.to.toLocalTime());
+        return newTimeSheetItem;
+    }
+}
