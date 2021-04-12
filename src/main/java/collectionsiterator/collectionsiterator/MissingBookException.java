9237335f92adec8341805a0a8cd3790a13328928
lippaitamas1021 <lippaitamas1021@gmail.com>
2021-04-12 11:18:16 +0200

    collectionsiterator.collectionsiterator commit

---
+++ src/main/java/collectionsiterator/collectionsiterator/MissingBookException.java
@@ -0,0 +1,15 @@
+package collectionsiterator.collectionsiterator;
+
+public class MissingBookException extends RuntimeException {
+
+    private String message;
+
+    public MissingBookException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
